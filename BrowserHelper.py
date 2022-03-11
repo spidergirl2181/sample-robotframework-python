db1f5bb27b50f70b4a2b17b88220eecaf266694a
spidergirl2181 <97619215+spidergirl2181@users.noreply.github.com>
2022-03-11 13:37:05 +0700

    Update code format of BrowserHelper

--- BrowserHelper.py
+++ BrowserHelper.py
@@ -6,19 +6,19 @@ class BrowserHelper(object):
 
 	def __init__(self):
 		option = webdriver.ChromeOptions()
-    	option.add_argument("--start-maximized")
+    		option.add_argument("--start-maximized")
     # start of headless mode config
-    	option.add_argument("--headless")
-    	option.add_argument("--disable-gpu")
-    	option.add_argument("--disable-dev-shm-usage")
-    	option.add_argument("--no-sandbox")
-    	option.add_argument("--window-size=1920,1080")
-    	option.add_argument("--no-first-run")
-    	option.add_argument("--no-default-browser-check")
-    	option.add_argument("--ignore-certificate-errors")
+    		option.add_argument("--headless")
+    		option.add_argument("--disable-gpu")
+    		option.add_argument("--disable-dev-shm-usage")
+    		option.add_argument("--no-sandbox")
+    		option.add_argument("--window-size=1920,1080")
+    		option.add_argument("--no-first-run")
+    		option.add_argument("--no-default-browser-check")
+    		option.add_argument("--ignore-certificate-errors")
     #end of headless mode config
-    	self.ChromeDriver = webdriver.Chrome(r'chromedriver.exe', chrome_options=option)
-    	self.implicit_wait = 50
+    		self.ChromeDriver = webdriver.Chrome(r'chromedriver.exe', chrome_options=option)
+    		self.implicit_wait = 50
 
 	def access_to_url(self, url):
 		self.ChromeDriver.get(url)
